06fb83181790e5c7179ee79c7c44ac8466c4f79d
martin <nekhavhambe308@gmail.com>
2024-09-04 08:32:08 +0200

    yes

--- app/spending/Invoice.tsx
+++ app/spending/Invoice.tsx
@@ -29,7 +29,7 @@ const InvoiceList = ({ invoices }) => {
             {invoices[0].map(invoice => {
               console.log(invoice, '<<<<<<<<<<<<------------>>>>>>>>>>>>>>>')
                 if(invoice){
-                    return  ( <tr style={{ height:35,backgroundColor:"#ccc"}} key={invoice.id}>
+                    return  ( <tr style={{ height:35,backgroundColor:"#D3D3D3"}} key={invoice.id}>
                         <td style={{width:250, borderTop:'1px solid black',  borderBottom:'1px solid black'}}>{invoice.created_from}</td>
                         <td  style={{width:150, borderTop:'1px solid black',  borderBottom:'1px solid black'}}>{invoice.date}</td>
                         <td  style={{width:150, textAlign:"left", borderTop:'1px solid black',  borderBottom:'1px solid black' }}>{invoice.vendor}</td>
