8c46ff586e19915137a87cc42e8e63c281d3921c
martin <nekhavhambe308@gmail.com>
2024-09-04 20:57:38 +0200

    yes

--- app/spending/Invoice.tsx
+++ app/spending/Invoice.tsx
@@ -31,11 +31,11 @@ const InvoiceList = ({ invoices }) => {
               console.log(invoice, '<<<<<<<<<<<<------------>>>>>>>>>>>>>>>')
                 if(invoice){
                     return  ( <tr style={{ height:35,backgroundColor:"#f9f9f9"}} key={invoice.id}>
-                        <td style={{width:250, borderTop:'1px solid black',  borderBottom:'1px solid black'}}>{invoice.created_from}</td>
-                        <td  style={{width:150, borderTop:'1px solid black',  borderBottom:'1px solid black'}}>{invoice.date}</td>
-                        <td  style={{width:150, textAlign:"left", borderTop:'1px solid black',  borderBottom:'1px solid black' }}>{invoice.vendor}</td>
-                        <td  style={{width:150, textAlign:"left", borderTop:'1px solid black',  borderBottom:'1px solid black' }}>{invoice.item}</td>
-                        <td  style={{width:150, borderTop:'1px solid black',  borderBottom:'1px solid black' }}>{invoice.amount}</td>
+                        <td style={{width:250, borderTop:'1px solid black',  borderBottom:'1px solid black', fontSize:14}}>{invoice.created_from}</td>
+                        <td  style={{width:150, borderTop:'1px solid black',  borderBottom:'1px solid black', fontSize:14}}>{invoice.date}</td>
+                        <td  style={{width:150, textAlign:"left", borderTop:'1px solid black',  borderBottom:'1px solid black', fontSize:14 }}>{invoice.vendor}</td>
+                        <td  style={{width:150, textAlign:"left", borderTop:'1px solid black',  borderBottom:'1px solid black', fontSize:14 }}>{invoice.item}</td>
+                        <td  style={{width:150, borderTop:'1px solid black',  borderBottom:'1px solid black', fontSize:14 }}>{invoice.amount}</td>
                       </tr>)
                 }
 
