21b50c602b97dbfc59139348e11317471755f1bc
martin <nekhavhambe308@gmail.com>
2024-09-12 21:43:23 +0200

    yes

--- app/table/Input.tsx
+++ app/table/Input.tsx
@@ -49,6 +49,7 @@ export default function textEditor({
       ref={autoFocusAndSelect}
       value={row[column.key]}
       onChange={(event) => {
+        document.querySelector('#save').style.display = 'flex'
         onRowChange({ ...row, [column.key]: event.target.value })}}
       onBlur={(e) => { 
         onClose(true, false)}}
