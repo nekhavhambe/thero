b46a207f261a88a49d285d6b55698cd3de43b592
martin <nekhavhambe308@gmail.com>
2024-09-13 00:09:01 +0200

    yes

--- app/spending/page.tsx
+++ app/spending/page.tsx
@@ -729,7 +729,7 @@ export default function ColumnGrouping({}) {
                   (115 / 100);
               }
 
-              let diff = (actual - totoal_budget) * -1 != 0 ? (actual - totoal_budget) * -1 : 0.00;
+              let diff = (totoal_budget - actual) 
               return new Intl.NumberFormat("en-US", {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
